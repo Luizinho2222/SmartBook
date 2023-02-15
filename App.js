edbe1808824b8735fa252db597d81ec6238d1dfc
Luizinho2222 <106774934+Luizinho2222@users.noreply.github.com>
2023-02-15 17:42:22 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import * as React from 'react' 
+import {component} from 'react';
+import * as Font from "expo-font";
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+import Constants from 'expo-constants';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        SMARTBOOK
+      </Text>
+      <Card>
+        <AssetExample />
+      </Card>
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
